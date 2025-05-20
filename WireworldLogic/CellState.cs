9ed32e559bc6872ef78835ca1b8cad28bc23f36f
Edgardo Ortiz <edgardo.ortiz@alumnos.ucn.cl>
2025-05-20 17:26:47 -0400

    Agregar archivos de proyecto.

---
+++ WireworldLogic/CellState.cs
@@ -0,0 +1,11 @@
+﻿namespace Wireworld.WireworldLogic
+{
+    enum CellState
+    {
+        Empty, // Empty -> Empty
+        ElectronHead, // Electron Head -> Electron Tail
+        ElectronTail, // Electron Tail -> Conductor
+        // Conductor -> Electron Head si tiene exactamente 1 o 2 vecinos ElectronHead
+        Conductor
+    }
+}
---
+++ WireworldLogic/Grid.cs
@@ -0,0 +1,11 @@
+﻿namespace Wireworld.WireworldLogic
+{
+    internal class Grid
+    {
+        // Es preferible usar un array 2D en vez de una lista de listas,
+        // ya que es más eficiente para matrices de tamaño estáticas
+        private CellState[,] grid;
+        private int Rows => grid.GetLength(0);
+        private int Cols => grid.GetLength(1);
+    }
+}
